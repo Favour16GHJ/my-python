210af477bfd67ae5a10caf39cc2223c4fc5cce05
Favour16GHJ <chibros08@gmail.com>
2025-10-21 13:09:30 +0100

    commit

--- BMI-category.py
+++ BMI-category.py
@@ -12,18 +12,18 @@ height = float(input("Enter your height in meters: "))
 
 bmi = weight / (height ** 2)
 # bmi_1dp = f"{bmi:.1f}"
-print(f"Your BMI is {bmi:.1f}")
+# print(f"Your BMI is {bmi:.1f}")
 
 if bmi >= 30.0:
-    print("Person is Obese")
+    print(f"Person is Obese with BMI of {bmi:.1f}")
 elif bmi < 30.0 and bmi >= 25.0:
-    print("Person is Overweight")
+    print("Person is Overweight with BMI of {bmi:.1f}")
 elif bmi < 25.0 and bmi  >= 18.5:
-    print("Person is of Healthy Weight")
+    print("Person is of Healthy Weight with BMI of {bmi:.1f}")
 elif bmi < 18.5:
-    print("Person is Underweight")
+    print("Person is Underweight with BMI of {bmi:.1f}")
 else:
-    print("Recheck Algorithm")
+    print("Recheck Algorithm with BMI of {bmi:.1f}")
 
 # num = float(input("num: "))
 # num_2dp = f"{num:.1f}"
---
+++ while-loop.py
@@ -0,0 +1,10 @@
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+print("Loop 1 ended.")
+
+a = 5
+while a > 0:
+    print(a)
+    a -= 1
