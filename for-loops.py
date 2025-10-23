daa01162ccfa2bac360c023cb3942e1355e09c5e
Favour16GHJ <chibros08@gmail.com>
2025-10-23 13:37:41 +0100

    commit

---
+++ for-loops.py
@@ -0,0 +1,42 @@
+# x = "Andrew"
+# for i in x:
+#     print(i)
+
+# x = ["You", "Me", "Us"]
+# for i in x:
+#     print(i)
+
+# # range(starting, ending, step)
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(1,5):
+#     print(i)
+
+# for i in range(1, 5, 2):
+#     print(i)
+
+
+# value = int(input("Enter value to multiply: "))
+# for i in range(1, 13):
+#     print(f"{value} x {i} = {value*i}")
+
+# for i in range(5):
+#     print(i)
+# else: 
+#     print("It is finished.")
+
+
+
+# # Nested For loops
+# fruits = ["Apple", "Banana",  "Cherry", "Dates"]
+# colors = ["Red", "Green", "Blue", "Brown"]
+
+# for i in fruits:
+#     for a in colors:
+#         print(f' {i} is {a}')
+user = int(input("Enter number of stars: "))
+for i in range(1, user+1):
+    print("*"*i)
+
